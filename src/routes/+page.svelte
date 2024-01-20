d3555647e52fc89d84bfa80f2333529ad1b7e95e
Glenn Russell <glennadjrussell@gmail.com>
2024-01-20 08:55:09 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Greet from '../lib/Greet.svelte'
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<Greet/>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="/dashboard">kit.svelte.dev</a> to view the dashboard</p>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+  <link rel="stylesheet" href="bootstrap.min.css">
+  <script src="color-modes.js"></script>
+</svelte:head>
+
+<div>
+  <h2>Dashboard</h2>
+</div>
+
