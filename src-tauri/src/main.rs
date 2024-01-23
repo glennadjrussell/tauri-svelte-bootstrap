cbf5ed1304b16ff7722fa9e22e1837ad69d352b9
Glenn Russell <glennadjrussell@gmail.com>
2024-01-23 00:40:53 +0000

    feat: adding basic screen layout

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,33 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::fs;
+use std::path::Path;
+
+#[tauri::command]
+fn discover_network() {
+    let tcp_file = Path::new("/proc/net/tcp");
+    let data = match fs::read_to_string(tcp_file) {
+        Ok(data) => data,
+        Err(err) => {
+            eprintln!("Error reading /proc/net/tcp: {}", err);
+            return;
+        }
+    };
+
+    for line in data.lines().skip(1) {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        if parts.len() >= 11 {
+            let local_address = parts[1];
+            let remote_address = parts[2];
+            let inode = parts[9];
+
+            println!("Local: {}, Remote: {}, Inode: {}", local_address, remote_address, inode);
+            // You can use the inode to find more information about the connection, e.g., in /proc/<pid>/fd/
+        }
+    }
+}
+
 #[tauri::command]
 fn greet(name: &str) -> String {
   format!("Hello, {}!", name)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,126 @@
+<svelte:head>
+  <script src="/js/color-modes.js"></script>
+  <script src="/js/dashboard.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp" crossorigin="anonymous"></script>
+</svelte:head>
+
 <script>
-  import Greet from '../lib/Greet.svelte'
+  import { Container, Row, Col } from '@sveltestrap/sveltestrap';
+  import { Nav, NavItem, Navbar, NavbarBrand, NavLink } from '@sveltestrap/sveltestrap';
+  import { Table } from '@sveltestrap/sveltestrap';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<Greet/>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>Visit <a href="/dashboard">kit.svelte.dev</a> to view the dashboard</p>
+<Navbar color="light">
+  <NavbarBrand href="/">Metasast</NavbarBrand>
+</Navbar>
+
+<Container fluid>
+  <Row>
+    <Col md="3">
+    <Nav vertical>
+      <NavItem>
+        <NavLink href="#">
+	  <svg class="bi">
+	    <use xlink:href="/assets/icons.svg#house-fill"></use>
+	  </svg>
+	  Dashboard
+	</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">
+	  <svg class="bi">
+	    <use xlink:href="/assets/icons.svg#file-earmark"></use>
+	  </svg>
+	  Orders
+	</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">
+	  <svg class="bi">
+	    <use xlink:href="/assets/icons.svg#cart"></use>
+	  </svg>
+	  Products
+	</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">
+	  <svg class="bi">
+	    <use xlink:href="/assets/icons.svg#people"></use>
+	  </svg>
+	  Customers
+	</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">
+	  <svg class="bi">
+	    <use xlink:href="/assets/icons.svg#graph-up"></use>
+	  </svg>
+	  Reports
+	</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">
+	  <svg class="bi">
+	    <use xlink:href="/assets/icons.svg#puzzle"></use>
+	  </svg>
+	  Integrations
+	</NavLink>
+      </NavItem>
+    </Nav>
+    <h6><span>Saved reports</span></h6>
+    <Nav vertical>
+      <NavItem>
+        <NavLink href="#">Current month</NavLink>
+      </NavItem>
+    </Nav>
+    <hr class="my-3"/>
+    <Nav vertical>
+      <NavItem>
+        <NavLink href="#">
+	  <svg class="bi">
+	    <use xlink:href="/assets/icons.svg#gear-wide-connected"></use>
+	  </svg>
+	  Settings
+	</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">
+	  <svg class="bi">
+	    <use xlink:href="/assets/icons.svg#door-closed"></use>
+	  </svg>
+	  Sign out
+	</NavLink>
+      </NavItem>
+    </Nav>
+    </Col>
+    <Col md="9">
+      <main>
+        <h1 class="h2">Dashboard</h1>
+
+	<canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+
+	<h2>Section title</h2>
+	<Table striped size="sm">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Header</th>
+              <th scope="col">Header</th>
+              <th scope="col">Header</th>
+              <th scope="col">Header</th>
+            </tr>
+          </thead>
+
+	</Table>
+      </main>
+    </Col>
+  </Row>
+</Container>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css" rel="stylesheet">
+<link rel="stylesheet" href="/styles/dashboard.css"/>
+<link rel="stylesheet" href="/styles/mainpage.css"/>
+<link rel="stylesheet" href="/styles/bootstrap.min.css"/>
+<link rel="stylesheet" href="/styles/bootstrap.min.css.map"/>
+
